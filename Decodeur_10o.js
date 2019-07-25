331370a61c2fc6963e4aca9b8c765afe0a37b3de
Guiguitant Théo <theo.guiguitant@gmail.com>
2019-07-25 11:42:50 +0200

    update readme

--- Decodeur_10o.js
+++ Decodeur_10o.js
@@ -1,14 +1,14 @@
 function readbool(buf,offset){
-   offset = offset >>> 0;
+   offset = offset;
    if(buf[offset]==1){
-     return true
+     return 0
    }
     else{
-     return false
+     return 1
     }
 }
 function readInt64BE(buf, offset) {
-    offset = offset >>> 0;
+    offset = offset;
     val =(buf[offset]<<56) | (buf[offset+1]<<48) | (buf[offset+2]<<40) | (buf[offset+3]<<32) | ( buf[offset+4]<<24) | (buf[offset+5]<<16)  | (buf[offset + 6]<<8) | buf[offset+7];
 
     if (!(buf[offset] & 0x80)) {
@@ -17,7 +17,7 @@ function readInt64BE(buf, offset) {
     return ((~val)+1)*-1;
 }
 function readInt32BE(buf, offset) {
-    offset = offset >>> 0;
+    offset = offset;
     val = (buf[offset]<<24) | (buf[offset+1]<<16)  | (buf[offset + 2]<<8) | buf[offset+3] ;
 
     if (!(buf[offset] & 0x80)) {
@@ -27,7 +27,7 @@ function readInt32BE(buf, offset) {
 }
 
 function readInt16BE(buf, offset) {
-    offset = offset >>> 0;
+    offset = offset;
     val = (buf[offset]<<8) | buf[offset+1] ;
 
     if (!(buf[offset] & 0x80)) {
@@ -37,87 +37,69 @@ function readInt16BE(buf, offset) {
 }
 
 function readInt8BE(buf, offset) {
-    offset = offset >>> 0;
+    offset = offset;
     if (!(buf[offset] & 0x80)) {
         return (buf[offset])
     }
     return ((~buf[offset]) + 1) * -1;
 }
 function readUInt64BE(buf, offset) {
-    offset = offset >>> 0;
+    offset = offset;
     return (buf[offset]<<56) | (buf[offset+1]<<48) | (buf[offset+2]<<40) | (buf[offset+3]<<32) | ( buf[offset+4]<<24) | (buf[offset+5]<<16)  | (buf[offset + 6]<<8) | buf[offset+7];
 }
 function readUInt32BE(buf, offset) {
-    offset = offset >>> 0;
+    offset = offset;
     return (buf[offset]<<24) | (buf[offset+1]<<16)  | (buf[offset + 2]<<8) | buf[offset+3] ;
 }
 function readUInt16BE(buf, offset) {
-    offset = offset >>> 0;
+    offset = offset;
     return (buf[offset] << 8) | buf[offset + 1]
 }
 
 function readUInt8(buf, offset) {
-    offset = offset >>> 0
+    offset = offset;
     return (buf[offset])
 }
+function readUInt4H(buf, offset) {
+    offset = offset;
+    return (buf[offset]>>4)
+}
+function readUInt4L(buf, offset) {
+    offset = offset;
+    return (buf[offset] && ~(0xF<<4))
+}
+function readUInt2H(buf, offset) {
+    offset = offset;
+    return (buf[offset]>>6)
+}
+function readUInt6L(buf, offset) {
+    offset = offset;
+    return (buf[offset] & ~(0x3<<6))
+}
+
 
 Elsys_Payload = {
 
     'decodeUp': function (port, payload) {
-
-        var TYPE_TEMPERATURE = 0x01;
-        var TYPE_LONGITUDE=0x02;
-        var TYPE_LATITUDE=0x03;
-        var TYPE_HAUTEUR=0x05;
-        var TYPE_DEBIT=0x06;
-        var TYPE_DETECT=0x07
-        
-        // TBC
-
         var value = {}
         var idx = 0;
         var v;
-        while (idx < payload.length) {
-            dataType = payload[idx++];
-            // console.log(idx, "-->", dataType);
-            switch (dataType) {
-
-                case TYPE_TEMPERATURE:
-                      v = readInt16BE(payload, idx) / 10.0;
-                      idx += 2;
-                      value["temperature"] = v;
-                      break;
-                case TYPE_LONGITUDE:
-                      v = readInt32BE(payload, idx) / 100000.0;
-                      idx += 4;
-                      value["longitude"]= v;
-                      break;
-                case TYPE_LATITUDE:
-                      v = readInt32BE(payload, idx) / 100000.0;
-                      idx += 4;
-                      value["latitude"] = v;
-                      break;
-                case TYPE_HAUTEUR:
-                      v = readUInt16BE(payload, idx) / 10.0;
-                      idx += 2;
-                      value["temperature"] = v;
-                      break;
-                case TYPE_DEBIT:
-                      v = readUInt16BE(payload, idx) / 10.0;
-                      idx += 2;
-                      value["temperature"] = v;
-                      break;
-                case TYPE_DETECT:
-                      v = readUInt8(payload, idx);
-                      idx += 1;
-                      value["detection"] = v;
-                      break;
-                default:
-            }
+        v = readUInt2H(payload, idx);
+        value["detection"] = v;
+        v = readUInt6L(payload, idx);
+        value["batterie"] = v*0.2;
+        if(payload.length>1){
+                idx += 1;
+                v = readInt32BE(payload, idx) / 100000.0;
+                idx += 4;
+                value["longitude"]= v;
+                v = readInt32BE(payload, idx) /  100000.0;
+                idx += 4;
+                value["latitude"] = v;
         }
         return value;
-
     },
+
 
     'encodeDn': function (port, value) {
         // TO BE IMPLEMENTED
--- README.md
+++ README.md
@@ -1,19 +1,51 @@
 # Decodeur_Trame_LoRA
-This decoder is designed for use with TTN. 
-it must be placed ```payload Formats->decoder```
-il est prévus pour des trame contenant les information:
+These decoders are designed to process frames arriving on the TTN network.
+they must be placed in "payload Formats->decoder" of the TTN application
+it is intended for frames containing the information:
 * longitude
 * latitude
 * detection value
 * battery level value
+
 ## Version
 ### Decodeur_full
 it is the most classic version of each information and precede it with its identifier and the order does not matter nor the presence or not of all the information. This is the most flexible version. 
-### Decodeur_10
+
+### Decodeur_10(To be used for the application""""Supagrohydaulic""")
 version without identifiers. the order must therefore be respected imperatively.
+it accepts 2 types of messages:
+-heartbeat (only alert and battery)
+-full message (longitude and latitude in addition)
 ### decodeur_7
 planned for soon
 decoder reducing the geographical area of GPS coordinates. merging of detection and battery value 
 ### decodeur_5
 reduced to an area of about 100 km 
- 
+## fonction ```read```
+several functions are declared for reading data of different size and type 
+### readInt64BE and readUInt64BE 
+these 2 functions read 8 bytes the idx will have to advance by 8 after using them
+It's big edian coding. So we start with the most significant bytes. 
+```idx +=8 ; ```
+### readInt32BE and readUInt32BE 
+```idx +=4 ; ```
+### readInt16BE and readInt16BE 
+```idx +=2 ; ```
+###readInt8 and readInt8
+```idx +=1 ; ```
+### readUInt4H and readUInt4L
+it is 2 function is used in a row without advancing the idx. the first reads the 4 most significant bits, the second the least significant 4 bits. 
+```idx=1 after the 2```
+
+### readUInt2H and readUInt2L
+```
+ 2 MSB and 6 LSB
+idx=1 after the 2 
+```
+### readbool
+```
+idx=1
+return a Boolean 
+```
+it is possible to build from these functions all the formats you want 
+to send float. multiply the data by the correct power of 10 to obtain an integer. Then we just have to divide during decoding.
